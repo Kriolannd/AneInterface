01abb1666371e6175b15449f1c2596d4e0f6ba7b
Barsegyan Karen <Kriolannd@yandex.ru>
2024-02-01 15:19:34 +0300

    Initialized project

---
+++ src/main/java/com/example/ane/Main.java
@@ -0,0 +1,18 @@
+package com.example.ane;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/ane/MeshGrid.java
@@ -0,0 +1,2 @@
+package com.example.ane;public class MeshGrid {
+}
